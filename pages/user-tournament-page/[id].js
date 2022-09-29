7a6d8e78b8fd25b5e5a604af5fac52a35cf97acb
Carl Cronsioe <carlcro@kth.se>
2022-09-29 22:25:19 +0200

    Server side rendered highscore

--- pages/user-tournament-page/[id].js
+++ pages/user-tournament-page/[id].js
@@ -6,6 +6,7 @@ import dynamic from "next/dynamic";
 import HighScoreTable from "../../components/user-tournament-page/HighScoreTable";
 import { getHighscore } from "../../services/userTournamentService";
 import Spinner from "../../components/Spinner";
+import UserTournament from "../../models/user-tournament";
 
 const DynamicUserTournamentPanel = dynamic(
   () => import("../../components/user-tournament-page/UserTournamentPanel"),
@@ -14,32 +15,18 @@ const DynamicUserTournamentPanel = dynamic(
   }
 );
 
-const UserTournamentPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const { data = [], isLoading } = useQuery(
-    ["highscoreData", id],
-    () => getHighscore(id),
-    {
-      enabled: Boolean(id),
-    }
-  );
-
-  if (isLoading) {
-    return <Spinner />;
-  }
-
+const UserTournamentPage = ({ highscoreData }) => {
   return (
     <div className="flex flex-col-reverse md:flex-row md:space-x-8 px-5 items-center md:items-start">
       <DynamicUserTournamentPanel />
-      <HighScoreTable highscoreData={data} />
+      <HighScoreTable highscoreData={highscoreData} />
     </div>
   );
 };
 
 export default UserTournamentPage;
 
-/* export async function getServerSideProps({ params }) {
+export async function getServerSideProps({ params }) {
   const userTournament = await UserTournament.findById(params.id).populate({
     path: "members",
     populate: [{ path: "betSlip" }],
@@ -50,7 +37,7 @@ export default UserTournamentPage;
     .map((x) => ({
       id: x._id.toString(),
       fullName: x.fullName,
-      points: x.betSlip.points || 0,
+      points: x?.betSlip?.points || "-",
     }));
 
   return {
@@ -58,4 +45,4 @@ export default UserTournamentPage;
       highscoreData,
     },
   };
-} */
+}
