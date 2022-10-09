479c40eba94d4390fb72ae0489e7f65ce86bf2f5
Carl Cronsioe <carlcro@kth.se>
2022-10-09 16:05:33 +0200

    style

--- components/Match.js
+++ components/Match.js
@@ -93,7 +93,7 @@ const Match = ({ match, matchInfo, finalsStage, mode }) => {
             : "mb-1 grid grid-cols-[minmax(90px,_1fr)_120px_minmax(90px,_1fr)_30px] md:grid-cols-[85px_minmax(110px,_1fr)_110px_minmax(110px,_1fr)_40px] lg:grid-cols-[100px_repeat(3,_130px)_80px_1fr]"
           : finalsStage
           ? "mb-1 grid grid-cols-4 md:grid-cols-5 lg:grid-cols-[100px_150px_90px_90px_150px_1fr]"
-          : "mb-1 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-[100px_repeat(3,_150px)_1fr]"
+          : "mb-1 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-[100px_repeat(3,_160px)_1fr]"
       }
     >
       <div className="hidden md:flex md:flex-col items-center justify-center">
