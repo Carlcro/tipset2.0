6d8be841d3c92529a3d1622009faa4761abd3db0
Carl Cronsioe <carlcro@kth.se>
2022-10-19 22:39:31 +0200

    Tiebreakers

--- components/bet-slip/TiebreakerInfo.js
+++ components/bet-slip/TiebreakerInfo.js
@@ -1,46 +1,44 @@
 import React from "react";
 
 function TiebreakerInfo() {
+  const rules = [
+    "Antal inspelade poäng sett till samtliga spelade gruppspelsmatcher",
+    "Målskillnad sett till samtliga spelade gruppspelsmatcher",
+    "Antal gjorda mål sett till samtliga spelade gruppspelsmatcher",
+    "Antal inspelade poäng sett till matcherna mellan lagen i fråga",
+    "Målskillnad sett till matcherna mellan lagen i fråga",
+    "Antal gjorda mål sett till matcherna mellan lagen i fråga",
+  ];
+
   return (
     <div className="bg-white shadow-lg rounded px-10 py-2 m-2">
       <h1 className="font-bold text-lg mb-2 -ml-5 ">
         Tiebreakers i gruppspelet
       </h1>
       <ol className="list-decimal space-y-2">
-        <li>
-          <span className="font-bold">Högre antal inspelade poäng,</span>
-          (3p för vinst, 1p för oavgjort, 0p för förlust) i matcherna som
-          spelats lagen emellan
-        </li>
-        <li>
-          <span className="font-bold">Bättre målskillnad, </span>
-          sett till matcherna som spelats lagen emellan
-        </li>
-        <li>
-          <span className="font-bold">Fler gjorda mål, </span>
-          sett till matcherna som spelats lagen emellan
-        </li>
-        <li>
-          Om det, efter att ha applicerat kriterium 1-3, fortfarande finns lag
-          med likadan ranking ska 1-3 återigen appliceras för dessa lag
-          uteslutande. Om detta heller inte kan särskilja lagens ranking ska
-          kriterium 5-10 appliceras
-        </li>
-        <li>
-          <span className="font-bold">Bättre målskillnad </span>i samtliga
-          gruppspelsmatcher
-        </li>
-        <li>
-          <span className="font-bold">Fler gjorda mål </span>i samtliga
-          gruppspelsmatcher
-        </li>
+        {rules.map((rule, index) => (
+          <li key={index}>{rule}</li>
+        ))}
       </ol>
       <div className="-ml-5 mt-2 italic">
         OBS! Tipset tar inte hänsyn till rangordning utöver ovanstående
-        kriterier
+        kriterier. Vid tippning där lag fortfarande inte kan särskiljas efter
+        kriterier 1-6 kommer alfabetisk ordning avgöra.
       </div>
     </div>
   );
 }
 
 export default TiebreakerInfo;
+/*
+
+
+
+1. Antal inspelade poäng sett till samtliga spelade gruppspelsmatcher;
+2. Målskillnad sett till samtliga spelade gruppspelsmatcher;
+3. Antal gjorda mål sett till samtliga spelade gruppspelsmatcher;
+4. Antal inspelade poäng sett till matcherna mellan lagen i fråga;
+5. Målskillnad sett till matcherna mellan lagen i fråga;
+6. Antal gjorda mål sett till matcherna mellan lagen i fråga;
+
+*/
