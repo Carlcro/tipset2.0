e6ccbdd11f35808e61d89ea904a54443d8408de7
Carl Cronsioe <carlcro@kth.se>
2022-11-07 21:16:40 +0100

    asd

--- components/bet-slip/BetSlip.js
+++ components/bet-slip/BetSlip.js
@@ -73,7 +73,7 @@ const BetSlip = ({ mode, bettingAllowed, handleSave, setFinalsMatches }) => {
       {mode !== "placedBet" && (
         <Container classNames="mx-auto w-[300px] md:w-[500px] flex flex-col space-y-3 ">
           <h1 className="text-center text-xl">
-            Lägg tips för Världsmästerskapet i fotboll 2022
+            Lägg tips för Bröderna Duhlins VM-tips 2022
           </h1>
         </Container>
       )}
