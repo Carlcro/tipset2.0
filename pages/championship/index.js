4eb030a1736d89d1a1c84fd49485f5e19f154470
Carl Cronsioe <carlcro@kth.se>
2022-11-08 22:25:27 +0100

    Facit visar rätt

--- pages/championship/index.js
+++ pages/championship/index.js
@@ -4,6 +4,8 @@ import { setFromBetslipState } from "../../recoil/bet-slip/selectors/selectors";
 import { getAnswerSheet } from "../../services/championshipService";
 import dynamic from "next/dynamic";
 import Container from "../../components/Container";
+import { motion } from "framer-motion";
+import { getConfig } from "../../services/configService";
 
 const DynamicBetslip = dynamic(
   () => import("../../components/bet-slip/BetSlip"),
@@ -14,15 +16,27 @@ const DynamicBetslip = dynamic(
 const Championship = () => {
   const setFromBetslip = useSetRecoilState(setFromBetslipState);
 
-  useQuery(["answerSheet"], async () => {
-    const { data } = await getAnswerSheet();
-    if (data) {
-      setFromBetslip({
-        goalscorer: data.goalscorer?.player,
-        bets: [...data.results],
-      });
+  useQuery(
+    ["answerSheet"],
+    async () => {
+      const { data } = await getAnswerSheet();
+      if (data) {
+        setFromBetslip({
+          goalscorer: data.goalscorer?.player,
+          bets: [...data.results],
+        });
+      }
+    },
+    {
+      onError: () => {
+        setFromBetslip({
+          goalscorer: "",
+          bets: [],
+        });
+      },
+      retry: false,
     }
-  });
+  );
 
   return (
     <div className="pb-10">
